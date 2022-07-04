e4e8a3c800c50ceccec05a85191b95a2f8bc59b6
Nathan Doyle <oduilln@tcd.ie>
2022-07-04 01:19:47 +0100

    Finished 2022-01-18

--- R/22-01-18-chocolate.R
+++ R/22-01-18-chocolate.R
@@ -37,12 +37,25 @@ add_notches_as_lines <- function(plot, alpha = 1, size = 1, color = "red") {
         ), colour = color, size = size)
 }
 
+custom_boxplot <- function(data, x, y, nudge = 0.04, alpha = 0.2, xlab = NULL, ylab = NULL, title = NULL) { #nolint
+    plot <- ggplot(data) +
+        geom_boxplot(aes_string(x = x, y = y)) +
+        xlab({{xlab}}) +
+        ylab({{ylab}}) +
+        ggtitle({{title}})
+
+    plot <- add_notches_as_box(plot, nudge = nudge, alpha = alpha) +
+        theme_minimal()
+
+    return(plot)
+}
+
 # Load data ----
-tt <- tt_load("2022-01-18")
+# tt <- tt_load("2022-01-18")
 
-tt
+# tt
 
-data <- tt$chocolate
+data <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-18/chocolate.csv')
 
 # Data cleaning ----
 data <- data %>%
@@ -50,15 +63,15 @@ data <- data %>%
 
 data <- data %>%
     mutate(
-        BEANS = if_else(stringr::str_detect(ingredients, "B"), TRUE, FALSE),
+        BEANS = if_else(stringr::str_detect(ingredients, "B"), "Contains", "Does Not Contain"),
         SUGAR = if_else((stringr::str_detect(ingredients, "S") &
                              !stringr::str_detect(ingredients, "\\*") &
-                             !stringr::str_detect(ingredients, "\a")), TRUE, FALSE),
+                             !stringr::str_detect(ingredients, "\a")), TRUE, FALSE), # nolint
         SWEET = if_else(stringr::str_detect(ingredients, "S\\*"), TRUE, FALSE),
-        COCOA = if_else(stringr::str_detect(ingredients, "C"), TRUE, FALSE),
-        VANILLA = if_else(stringr::str_detect(ingredients, "V"), TRUE, FALSE),
-        LEC = if_else(stringr::str_detect(ingredients, "L"), TRUE, FALSE),
-        SALT = if_else(stringr::str_detect(ingredients, "Sa"), TRUE, FALSE),
+        COCOA = if_else(stringr::str_detect(ingredients, "C"), "Contains", "Does Not Contain"),
+        VANILLA = if_else(stringr::str_detect(ingredients, "V"), "Contains", "Does Not Contain"),
+        LECITHIN = if_else(stringr::str_detect(ingredients, "L"), "Contains", "Does Not Contain"),
+        SALT = if_else(stringr::str_detect(ingredients, "Sa"), "Contains", "Does Not Contain"),
     ) %>%
     mutate(sweetness = case_when(
         SUGAR == FALSE & SWEET == FALSE ~ "No Sweetener",
@@ -74,15 +87,18 @@ data %>%
     select(where(is.character)) %>%
     map(fct_count, sort = TRUE, prop = TRUE)
 
-plot1 <- data %>%
+data %>%
     ggplot(aes(y = rating, x = cocoa_percent)) +
     geom_point() +
     geom_smooth()
 
-data %>%
+plot1_cocoa <- data %>%
     ggplot(aes(y = rating, x = cocoa_percent)) +
     geom_jitter(alpha = 0.5) +
-    geom_smooth()
+    geom_smooth() +
+    xlab("Cocoa Level (%)") +
+    ylab("Review Rating") +
+    ggtitle("How Does Cocoa Affect Review Rating?")
 
 data %>%
     ggplot(aes(y = rating, x = cocoa_percent)) +
@@ -113,7 +129,6 @@ data %>%
     ggplot(aes(x = country_of_bean_origin, y = rating)) +
     geom_boxplot()
 
-
 plot2 <- data_no_na %>%
     ggplot() +
     geom_boxplot(
@@ -126,47 +141,67 @@ add_notches_as_box(plot2, alpha = 0.4, nudge = 0)
 
 p1 <- data_no_na %>%
     ggplot() +
-oxplot(aes(y = rating, x = sweetness),
-                 labels = c("None" = "NONE",
-                            "Other" = "OTHER",
-                            "Sugar" = "SUGAR"))
+    geom_boxplot(aes(y = rating, x = sweetness))
 
 add_notches_as_lines(p1)
 
 add_notches_as_box(p1, nudge = 0.04, alpha = 0.2) +
     theme_minimal()
 
-p <- data_no_na %>%
-    ggplot(aes(y = rating, x = sweetness, fill = sweetness)) +
-    geom_boxplot() +
-    scale_fill_brewer(palette = "Blues") +
-    theme_minimal()
+R <- map(
+    .x = list("sweetness", "VANILLA", "LECITHIN", "SALT"),
+    .f = ~ custom_boxplot(data_no_na, y = "rating", x = .x,
+        ylab = "Review Rating",
+        xlab = stringr::str_to_title(string = .x)))
 
-p2 <- data_no_na %>%
-    ggplot(aes(y = rating, x = BEANS)) +
-    geom_boxplot()
+gridExtra::grid.arrange(grobs = R, ncols = 2)
 
-p3 <- data_no_na %>%
-    ggplot(aes(y = rating, x = COCOA)) +
-    geom_boxplot()
 
-p4 <- data_no_na %>%
-    ggplot(aes(y = rating, x = VANILLA)) +
-    geom_boxplot()
+plot2_sweet <- custom_boxplot(data_no_na, x = "sweetness", y = "rating",
+    xlab = "Type of Sweetnener", ylab = "Review Rating",
+    title = "How Does Sweetener Affect Review Rating?")
 
-p5 <- data_no_na %>%
-    ggplot(aes(y = rating, x = LEC)) +
-    geom_boxplot()
+p1 <- custom_boxplot(data_no_na, x = "VANILLA", y = "rating",
+    xlab = "Vanilla", ylab = "Review Rating")
 
-p6 <- data_no_na %>%
-    ggplot(aes(y = rating, x = SALT)) +
-    geom_boxplot()
+p2 <- custom_boxplot(data_no_na, x = "LECITHIN", y = "rating",
+    xlab = "Lecithin")
 
-gridExtra::grid.arrange(p1, p2, p3, p4, p5, p6, nrow = 2)
-# Not much info in BEANS, so removing that plot;
-# also not a lot of info in COCOA, and if its removed we will
-# have a nice 2x2 grid
-gridExtra::grid.arrange(p1, p4, p5, p6, nrow = 2)
+p3 <- custom_boxplot(data_no_na, x = "SALT", y = "rating",
+    xlab = "Salt")
+
+gridExtra::grid.arrange(p1, p2, p3, nrow = 1, ncol = 3)
+# p <- data_no_na %>%
+#     ggplot(aes(y = rating, x = sweetness, fill = sweetness)) +
+#     geom_boxplot() +
+#     scale_fill_brewer(palette = "Blues") +
+#     theme_minimal()
+
+# p2 <- data_no_na %>%
+#     ggplot(aes(y = rating, x = BEANS)) +
+#     geom_boxplot()
+
+# p3 <- data_no_na %>%
+#     ggplot(aes(y = rating, x = COCOA)) +
+#     geom_boxplot()
+
+# p4 <- data_no_na %>%
+#     ggplot(aes(y = rating, x = VANILLA)) +
+#     geom_boxplot()
+
+# p5 <- data_no_na %>%
+#     ggplot(aes(y = rating, x = LECITHIN)) +
+#     geom_boxplot()
+
+# p6 <- data_no_na %>%
+#     ggplot(aes(y = rating, x = SALT)) +
+#     geom_boxplot()
+
+# gridExtra::grid.arrange(p1, p2, p3, p4, p5, p6, nrow = 2)
+# # Not much info in BEANS, so removing that plot;
+# # also not a lot of info in COCOA, and if its removed we will
+# # have a nice 2x2 grid
+# gridExtra::grid.arrange(p1, p4, p5, p6, nrow = 2)
 
 data_no_na %>%
     ggplot(aes(y = rating, x = sweetness)) +
@@ -181,7 +216,7 @@ data %>%
 
 ggplot(data) +
     geom_boxplot(aes(x = .panel_x, y = .panel_y, group = .panel_x)) +
-    facet_matrix(rows = vars(rating), cols = vars(COCOA, VANILLA, LEC))
+    facet_matrix(rows = vars(rating), cols = vars(COCOA, VANILLA, LECITHIN))
 
 data %>%
     drop_na() %>%
@@ -193,10 +228,10 @@ data %>%
  print(n = 22)
 
 data %>%
-    count(ingredients, BEANS, sweetness, COCOA, VANILLA, LEC, SALT)
+    count(ingredients, BEANS, sweetness, COCOA, VANILLA, LECITHIN, SALT)
 
 data %>%
-    select(BEANS, sweetness, COCOA, VANILLA, LEC, SALT) %>%
+    select(BEANS, sweetness, COCOA, VANILLA, LECITHIN, SALT) %>%
     map(~ fct_count(as.factor(.x)))
 
 
@@ -204,12 +239,20 @@ data %>%
  data %>%
     tidyr::drop_na() %>%
     rename(SWEETNESS = sweetness) %>%
-    select(rating, BEANS, SWEETNESS, COCOA, VANILLA, LEC, SALT) %>%
+    select(rating, BEANS, SWEETNESS, COCOA, VANILLA, LECITHIN, SALT) %>%
     mutate(across(!rating, .fns = as.character)) %>%
     pivot_longer(!rating, names_to = "INGR", values_to = "value") %>%
     gg_boxplot(x = value, y = rating, facet = INGR)
 
-#' TO DO;
-#' 1. data cleaning at start
-#' 2. put plots in seperate file?
-#' 3. can put process in markdown file
+path <- "plots/2022-01-18/"
+ggplot2::ggsave(filename = paste0(path, "cocoa.pdf"), plot = plot1_cocoa)
+ggplot2::ggsave(filename = paste0(path, "sweetness.pdf"), plot = plot2_sweet)
+pl3 <- gridExtra::arrangeGrob(p1, p2, p3, nrow = 1, ncol = 3, top = "Effect of Various Ingredients on Review Rating")
+ggplot2::ggsave(filename = paste0(path, "ingredients.pdf"), plot = pl3)
+
+for(name in c("cocoa", "sweetness", "ingredients")) {
+    pdftools::pdf_convert(
+        pdf = glue::glue("{path}{name}.pdf"), 
+        filenames = glue::glue("{path}{name}.png"),
+        format = "png", dpi = 450)
+}
---
+++ plots/2022-01-18/README.md
@@ -0,0 +1,5 @@
+### [2022-01-18 - Chocolate Ratings](https://github.com/oduilln/tidy-tuesday/R/22-01-18-chocolate.R)
+
+![./plots/2022-01-18/cocoa.png]()
+![./plots/2022-01-18/sweetness.png]()
+![./plots/2022-01-18/ingredients.png]()
