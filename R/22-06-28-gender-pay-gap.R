bc7a3bbef980a2632a186304159df4fc4d6df346
Nathan Doyle <oduilln@tcd.ie>
2022-07-09 18:22:46 +0100

    WIP

--- R/22-06-28-gender-pay-gap.R
+++ R/22-06-28-gender-pay-gap.R
@@ -5,4 +5,35 @@ paygap <- readr::read_csv(data_path)
 
 tidytuesdayR::tt_load(x = "2022-06-28")
 
+paygap <- paygap %>%
+  janitor::clean_names() %>%
+  mutate(across(c(due_date, date_submitted), lubridate::as_datetime),
+    employer_nameloyer_name = str_remove_all(employer_name, "\""),
+    employer_name = str_replace_all(employer_name, ", |,", ", "))
+
 paygap
+
+ggplot(paygap, aes(y = diff_median_hourly_percent,
+                   x = diff_mean_hourly_percent)) +
+geom_point()
+
+paygap %>% summary()
+
+paygap %>%
+    group_by(employer_id) %>%
+    summarise(min = min(due_date),
+              max = max(due_date),
+              n = n()) %>%
+              count(n)
+
+# Can look at distributions of mean/median differences
+ggplot(paygap, aes(x = diff_median_hourly_percent)) +
+ geom_histogram(bins = 100)
+
+ggplot(paygap, aes(x = diff_median_hourly_percent)) +
+ geom_histogram(bins = 100) +
+    ggplot2::xlim(-100, 100)
+
+# How many companies are men paid more?
+
+# Can look at change through time by using date (due/submitted)
